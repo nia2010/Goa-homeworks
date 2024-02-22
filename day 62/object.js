c15c22cfd506b2a41cc78ada9e5b1c5f39090531
niakhoperia <nia.khoperia9@gmail.com>
2024-02-23 01:12:27 +0400

    fourth commit

---
+++ day 62/object.js
@@ -0,0 +1,86 @@
+// let calculator = {
+    
+//     add: function(num1, num2) {
+//         return num1 + num2;
+//     },
+
+  
+//     subtract: function(num1, num2) {
+//         return num1 - num2;
+//     },
+
+   
+//     multiply: function(num1, num2) {
+//         return num1 * num2;
+//     },
+
+   
+//     divide: function(num1, num2) {
+      
+//         if (num2 !== 0) {
+//             return num1 / num2;
+//         } else {
+//             return "Cannot divide by zero";
+//         }
+//     }
+// };
+
+
+// console.log(calculator.add(5, 3)); 
+// console.log(calculator.subtract(10, 4)); 
+// console.log(calculator.multiply(2, 6));  
+// console.log(calculator.divide(15, 3));  
+// console.log(calculator.divide(10, 0));  
+
+
+// const school = {
+//     name: "second school",
+//     students: [
+//     {
+//     name: 'sofia',
+//     age: 16
+//     },
+//     {
+//     name: 'barbara',
+//     age: 17
+//     },
+//     {
+//     name: 'mate',
+//     age: 16
+//     }
+//     ]
+//     }
+//     console.log(school.students[0].name) console.log(school.students[0].age)
+
+// const person1 = {
+//     name: 'nia',
+//     age: 14
+//     }
+//     const person2 = {
+//     name: 'nia',
+//     age: 14
+//     }
+//     console.log(person1 === person2)
+
+// const person = {
+    
+//     name: 'barbare',
+//     age: 14,
+//     city: 'mtskheta',
+    
+//     greet: function(name){
+    
+//     console.log('your name is' + name)
+    
+//     }
+//     }
+//     person.greet('nia')
+    
+//     console.log(person.name)
+//     person.age = 15
+   
+//     console.log(person.age)
+    
+//     person.country = 'Georgia'
+    
+//     console.log(person.country)

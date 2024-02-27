4aed16ddd53449db69205985c32d72be6a448a4b
niakhoperia <nia.khoperia9@gmail.com>
2024-02-27 18:38:09 +0400

    commit

---
+++ day63/homework/script.js
@@ -0,0 +1,11 @@
+function Flight(flightCode, flightStatus) {
+    this.flightCode = flightCode;
+    this.flightStatus = flightStatus;
+}
+
+
+let flight1 = new Flight("ABC123", "On Time");
+let flight2 = new Flight("XYZ789", "Delayed");
+
+
+
